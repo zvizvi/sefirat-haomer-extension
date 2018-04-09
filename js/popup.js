d89e20a87d98ab7bb70dc2a1a3db48a7ee0dddc8
zvizvi <5633910@gmail.com>
2018-04-09 12:48:07 +0300

    Work on omer days show

--- js/popup.js
+++ js/popup.js
@@ -5,6 +5,22 @@ $(document).ready(function () {
 
   var today, todayHebrewObj, todayHebrew, todayOmer;
 
+  var numberLetterList = {
+    '1': 'אֶחָד',
+    '1a': 'אחד',
+    '2': 'שנים',
+    '2a': 'שְׁנֵי',
+    '3': 'שלשה',
+    '4': 'ארבעה',
+    '5': 'חמשה',
+    '6': 'ששה',
+    '7': 'שבעה',
+    '8': 'שמונה',
+    '9': 'תִּשְׁעָה',
+    '10': 'עשרה'
+  };
+  var sefiraList = ['חסד', 'גבורה', 'תפארת', 'נצח', 'הוד', 'יסוד', 'מלכות'];
+
   moment.locale('he');
 
   function setupDate () {
@@ -22,15 +38,21 @@ $(document).ready(function () {
     console.log(todayHebrewObj);
     console.log(todayHebrew);
     console.log(todayOmer);
+    var day = numberLetterList[todayOmer];
+    if (todayOmer <= 10) {
+      day += ' יָמִים';
+    } else {
+      day += ' יום';
+    }
+    $('.day').text(day);
   }
 
-  function getSefira (todayOmer) {
-    var list = ['חסד', 'גבורה', 'תפארת', 'נצח', 'הוד', 'יסוד', 'מלכות'];
-    var todaySefira = list[(todayOmer % 7) - 1] + ' שב' + list[Math.floor(todayOmer / 7)];
+  function getSefira () {
+    var todaySefira = sefiraList[(todayOmer % 7) - 1] + ' שב' + sefiraList[Math.floor(todayOmer / 7)];
     $('.sefira').text(todaySefira);
   }
 
   setupDate();
   getDays();
-  getSefira(todayOmer);
+  getSefira();
 });
