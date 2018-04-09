4eefb942d194f792ee3bfb10c394ba9bb3354f92
zvizvi <5633910@gmail.com>
2018-04-09 03:23:56 +0300

    Improvents

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,36 @@
 // popup.js
+$(document).ready(function () {
+  var moment = window.moment;
+  var Hebcal = window.Hebcal;
+
+  var today, todayHebrewObj, todayHebrew, todayOmer;
+
+  moment.locale('he');
+
+  function setupDate () {
+    today = moment().format();
+    todayHebrewObj = Hebcal.HDate(new Date());
+    todayHebrew = todayHebrewObj.toString('h');
+    todayOmer = todayHebrewObj.omer();
+
+    $('.week-day').text('יום ' + moment().format('dddd'));
+    $('.hebrew-date').text(todayHebrew);
+  }
+
+  function getDays () {
+    console.log(today);
+    console.log(todayHebrewObj);
+    console.log(todayHebrew);
+    console.log(todayOmer);
+  }
+
+  function getSefira (todayOmer) {
+    var list = ['חסד', 'גבורה', 'תפארת', 'נצח', 'הוד', 'יסוד', 'מלכות'];
+    var todaySefira = list[(todayOmer % 7) - 1] + ' שב' + list[Math.floor(todayOmer / 7)];
+    $('.sefira').text(todaySefira);
+  }
+
+  setupDate();
+  getDays();
+  getSefira(todayOmer);
+});
